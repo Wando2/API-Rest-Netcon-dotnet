0bda40a181fa1fa7cdd46a4aaf387474f1a27922
Wando2 <113200379+Wando2@users.noreply.github.com>
2023-10-26 09:29:48 -0300

    gitignore

--- API Rest Netcon dotnet/Tests/ConversorTets.cs
+++ API Rest Netcon dotnet/Tests/ConversorTets.cs
@@ -9,56 +9,56 @@ namespace API_Rest_Netcon_dotnet.Tests
         [Test]
         public void Conversor_Date_ShouldBeSet()
         {
-            // Arrange
+           
             var conversor = new Conversor();
             var expectedDate = DateTime.Now;
 
-            // Act
+            
             conversor.Date = expectedDate;
 
-            // Assert
+            
             Assert.AreEqual(expectedDate, conversor.Date);
         }
 
         [Test]
         public void Conversor_StatusCode_ShouldBeSet()
         {
-            // Arrange
+            
             var conversor = new Conversor();
             var expectedStatusCode = 200;
 
-            // Act
+            
             conversor.statusCode = expectedStatusCode;
 
-            // Assert
+            
             Assert.AreEqual(expectedStatusCode, conversor.statusCode);
         }
 
         [Test]
         public void Conversor_ErrorMessage_ShouldBeSet()
         {
-            // Arrange
+           
             var conversor = new Conversor();
             var expectedErrorMessage = "Error message";
 
-            // Act
+           
             conversor.errorMessage = expectedErrorMessage;
 
-            // Assert
+            
             Assert.AreEqual(expectedErrorMessage, conversor.errorMessage);
         }
 
         [Test]
         public void Conversor_Value_ShouldBeSet()
         {
-            // Arrange
+           
             var conversor = new Conversor();
             var expectedValue = 10.5f;
 
-            // Act
+       
             conversor.value = expectedValue;
 
-            // Assert
+          
             Assert.AreEqual(expectedValue, conversor.value);
         }
     }
